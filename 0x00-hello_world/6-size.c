0c370f937489bb7ab4d6bf1e393fb6b97189890f
KingHawkins <kinghawkins61@gmail.com>
2022-09-08 14:50:35 -0700

    changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: 8 byte(s)\n");
-	printf("Size of a float: %d bytes(s)\n", sizeof(float));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
