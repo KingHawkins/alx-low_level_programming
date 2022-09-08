5978fff6930a0c155075b5bd6edb7947bca3234d
KingHawkins <kinghawkins61@gmail.com>
2022-09-08 08:57:36 -0700

    getting started with puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * A program that prints a string
+ * return 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
