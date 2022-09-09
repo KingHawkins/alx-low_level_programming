52f83854b8f1e574cfe62ccf3f3bed0cfe094339
KingHawkins <kinghawkins61@gmail.com>
2022-09-08 19:49:58 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<unistd.h>
 /**
  * main - Entry point. Contains implementation of the code
  *
@@ -6,6 +7,6 @@
  **/
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
