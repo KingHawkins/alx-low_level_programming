d468e8f1c6ab89a49eed539e545096b3547c6006
KingHawkins <kinghawkins61@gmail.com>
2022-09-14 07:11:30 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - prints a string folloed by a new line.
+ *
+ * Return: Always 0 (success)
+ **/
+int main(void)
+{
+	_putchar("_putchar");
+	_putchar('\n');
+	return (0);
+}
