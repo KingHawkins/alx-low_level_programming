88725f32090ed78893074d7fc8afbce6dd1031e0
KingHawkins <kinghawkins61@gmail.com>
2022-09-14 09:41:45 -0700

    task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,18 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<ctype.h>
+/**
+ * entry point
+ */
+void print_alphabet(void)
+{
+	char ch[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	int i;
+
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		ch[i] = tolower(ch[i]);
+		putchar(ch[i]);
+	}
+}
+
