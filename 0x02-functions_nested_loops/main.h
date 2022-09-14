4ed855203fe6db48d7dfc838598ed84038f16a76
KingHawkins <kinghawkins61@gmail.com>
2022-09-14 07:29:47 -0700

    changes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,1 @@
-#include <unistd.h>
 #include<stdio.h>
-/**
- * _putchar - writes the charscter c to stdout.
- * @c: The character to print.
- *
- * Return: On success 1.
- * On error, -1 is returned.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
