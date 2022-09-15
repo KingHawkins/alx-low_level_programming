03b1e73a6b380f8fdfeede2a0fd868aa7ec5b671
KingHawkins <kinghawkins61@gmail.com>
2022-09-15 05:52:20 -0700

    changes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,7 +15,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int a);
-int print_last-digit(int n);
+int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
