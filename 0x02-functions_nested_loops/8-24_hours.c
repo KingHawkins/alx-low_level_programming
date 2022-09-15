3f1b9aee18715c108996a55bcc2b1a248d4e07ac
KingHawkins <kinghawkins61@gmail.com>
2022-09-15 14:14:29 -0700

    changes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,29 +1,46 @@
-#include"main.h"
-#include<stdio.h>
+#include "main.h"
+
 /**
- * jack_bauer - prints every minute of the day starting from 00:00 to 23:59
- *
- * Return: Always 0 (success)
- **/
+ *  * jack_bauer - prints every minute of the day
+ *   * Description: prints every minute of the day
+ *    * Return: void
+ *     */
 void jack_bauer(void)
 {
-	int i, k, n, j;
+		int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-	for (i = 0; i < 3; i++)
-	{
-		for (k = 0; k < 4; k++)
-		{
-			for (j = 0; j < 6; j++)
-			{
-				for (n = 0; n < 10; ++n)
-				{
-					if (i != 2 && k != 3 && j != 6 && n != 0)
-					{
-						printf("%d%d:%d%d", i, k, j, n);
-						printf("\n");
-					}
-				}
-			}
-		}
-	}
+			hours_max = 58;
+				hours_tens = '0';
+					while (hours_tens < '3')
+							{
+										if (hours_tens == '2')
+													{
+																	hours_max = '4';
+																			}
+												hours_ones = '0';
+														while (hours_ones < hours_max)
+																	{
+																					minutes_tens = '0';
+																								while (minutes_tens < '6')
+																												{
+																																	minutes_ones = '0';
+																																					while (minutes_ones < 58)
+																																										{
+																																																_putchar(hours_tens);
+																																																					_putchar(hours_ones);
+																																																										_putchar(':');
+																																																															_putchar(minutes_tens);
+																																																																				_putchar(minutes_ones);
+																																																																									_putchar('\n');
+																																																																														minutes_ones++;
+																																																																																		}
+																																									minutes_ones = '0';
+																																													minutes_tens++;
+																																																}
+																											minutes_tens = '0';
+																														hours_ones++;
+																																}
+																hours_ones = '0';
+																		hours_tens++;
+																			}
 }
