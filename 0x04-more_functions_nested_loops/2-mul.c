9e46e9751264a45de752deff0b2e372d313c4a52
KingHawkins <kinghawkins61@gmail.com>
2022-09-16 03:41:10 -0700

    adding the task two

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - prototype
+ * @a: parameter 1
+ * @b: parameter 2
+ * Description: returns the result of multiplication
+ * Return: 1 if success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
