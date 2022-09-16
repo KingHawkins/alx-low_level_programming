b280940f4a2eb4938961e50d68ef084f811652cc
KingHawkins <kinghawkins61@gmail.com>
2022-09-15 22:18:43 -0700

    changes

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,10 +7,10 @@
 int _putchar(int c);
 int _isupper(int c);
 int _isdigit(int c);
-void _print_numbers(void);
+void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print-line(int n);
+void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
