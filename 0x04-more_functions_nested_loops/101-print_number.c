9b9ccbc4e2152cc8161661d5dc5db2ddd923b531
KingHawkins <kinghawkins61@gmail.com>
2022-09-16 07:12:22 -0700

    changes to task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,14 +10,14 @@ void print_number(int n)
 	int d = 1;
 	unsigned int x = n;
 	unsigned int y = n;
-	int c;
+	int c = 0;
 
 
 	if (n == 0)
 	{
 		_putchar('0');
 	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		n = n + 1;
