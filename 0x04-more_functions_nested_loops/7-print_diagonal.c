c2455da271f8f196132f0de4ab1df415902139ff
KingHawkins <kinghawkins61@gmail.com>
2022-09-16 05:58:48 -0700

    task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagnal
+ * @n: parameter
+ *
+ * Description: prints a diagonal based on n instances
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar(" ");
+		}
+	}
+	_putchar('\');
+	_putchar('\n');
+}
