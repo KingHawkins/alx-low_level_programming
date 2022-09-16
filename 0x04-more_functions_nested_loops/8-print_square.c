5a49b35a432bbbd7e6cf4625c2caea471ec91801
KingHawkins <kinghawkins61@gmail.com>
2022-09-16 08:01:55 -0700

    task8 pushing

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ * @size: instances at which a square is printed
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (size > 0)
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+}
