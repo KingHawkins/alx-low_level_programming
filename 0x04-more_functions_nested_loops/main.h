77e63219b50399af40047169050654ec961eb9bc
KingHawkins <kinghawkins61@gmail.com>
2022-09-15 21:59:01 -0700

    changes

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,6 +14,7 @@ void print-line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
+void print_number(int n)i;
+int mul(int a, int b);
 
 #endif /* MAIN_H */
