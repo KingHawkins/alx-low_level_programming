112159893fbaf5281e081dd897f5c565571f3b69
KingHawkins <kinghawkins61@gmail.com>
2022-09-16 08:39:44 -0700

    changes

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,19 +8,18 @@ void print_square(int size)
 {
 	int i, j;
 
-	while (i < size && size > 0)
+	if (size <= 0)
 	{
-		j = 0;
-		while (j < size)
+		_putchar('\n');
+	}else
+	{
+		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
-			j++;
+			for (j = 0; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-		i++;
-	}
-	if (i == 0)
-	{
-		_putchar('\n');
 	}
 }
