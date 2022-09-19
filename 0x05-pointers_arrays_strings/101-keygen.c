034b2f4917d86f2791b60e1a5146e289aabb17ca
KingHawkins <kinghawkins61@gmail.com>
2022-09-19 06:35:59 -0700

    task11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Description: returns a andom number
+ * Return: 0 success
+ */
+int main(void)
+{
+	unsigned long int password;
+
+	srand(time(0));
+	password = rand() - RAND_MAX / 2;
+	printf("%lld", password);
+	return (0);
+}	
