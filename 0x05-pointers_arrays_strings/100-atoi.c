998959e2352a5c7f8cf0c7aaa1334ba7224f62a0
KingHawkins <kinghawkins61@gmail.com>
2022-09-19 05:36:35 -0700

    task10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _atoi - prototype
+ * @s: parameter
+ * Description: converts string to integeer
+ * Return: 0 (success)
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
