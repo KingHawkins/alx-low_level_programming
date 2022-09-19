bbca3ab0754c574a0c2b9899f10e1d90c579f252
KingHawkins <kinghawkins61@gmail.com>
2022-09-19 09:10:38 -0700

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_array - protottype
+ * @a: pointer to n.
+ * @n: parameter
+ * Description: prints elements of array of integers
+ */
+void print_array(int *a, int n)
+{
+	for (n[a] = 0; n[a] != NULL; *a++)
+	{
+		_putchar(n[a]);
+	}
+}
