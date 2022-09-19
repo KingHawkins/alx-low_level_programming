1cd09b05a193ea918de4f0b2ece4f774692fc7e8
KingHawkins <kinghawkins61@gmail.com>
2022-09-19 13:32:09 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,9 @@ void rev_string(char *s)
 	int i;
 	char t;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
 	for (i = strlen(s) - 1; i > -1; i--)
 	{
-		t = (s[i]);
+		t = s[i];
 	}
+	_putchar('\n');
 }
