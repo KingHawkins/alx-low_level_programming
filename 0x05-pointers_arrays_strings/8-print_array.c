8405adac82488cb7f640ff84aebbc9cd26650246
KingHawkins <kinghawkins61@gmail.com>
2022-09-19 12:59:07 -0700

    task5

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include<stddef.h>
+#include<stdio.h>
 /**
  * print_array - protottype
  * @a: pointer to n.
@@ -12,6 +12,6 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%ld", a[i]);
+		printf("%d", a[i]);
 	}
 }
