c084a1344163b10f733be1844a1abd72cf2b9605
KingHawkins <kinghawkins61@gmail.com>
2022-09-19 08:43:34 -0700

    task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,7 +10,7 @@ void puts_half(char *str)
 {
 	 unsigned int i;
 
-	for (i = floor((strlen(str) - 1) / 2); i <= strlen(str); i++)
+	for (i = floor((strlen(str) - 1) / 2) + 1; i <= strlen(str); i++)
 	{
 		_putchar(str[i]);
 	}
