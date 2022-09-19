27258697637b6fccd71f46f6992599dfa811c64a
KingHawkins <kinghawkins61@gmail.com>
2022-09-19 00:48:12 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - main function
+ * @a: parameter one
+ * @b: parameter two
+ *
+ * Description: swaps the values a and b
+ */
+void swap_int(int *a, int *b)
+{
+	a = &b;
+	*a = *b;
+	b = &a;
+	*b = *a;
+}
