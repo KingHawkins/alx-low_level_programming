d4247c205b996bc3d1a521c6da091fd70984198c
KingHawkins <kinghawkins61@gmail.com>
2022-09-19 07:47:08 -0700

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _strcpy - prototype
+ * @dest: parameter
+ * @src: parameter
+ * Description: copies string to another
+ * Return: 0 success
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char s1, s2;
+
+	*dest = s1;
+	*src = s2;
+	strcpy(s1, s2);
+	return (*dest);
+}
