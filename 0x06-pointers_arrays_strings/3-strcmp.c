0b06a996cafc12c559c7747561e9a18af3a06965
KingHawkins <kinghawkins61@gmail.com>
2022-09-22 00:23:24 -0700

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,19 +9,22 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (!(strcmp(s1, s2))
+	int a;
+
+	if (!(strcmp(s1, s2)))
 	{
-		return (0);
+		a = 0;
 	}
 	if (strcmp(s1, s2))
 	{
 		if (s1 > s2)
 		{
-			return (15);
+			a = 15;
 		}
 		else
 		{
-			return (-15);
+			a = -15;
 		}
 	}
+	return (a);
 }
