4634c3acff74e207274c8d7f052b9a5bc8e2a03c
KingHawkins <kinghawkins61@gmail.com>
2022-09-22 00:14:42 -0700

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - prototype
+ * @s1: first pointer parameter
+ * @s2: second pointer parameter
+ * Description: compares two strings
+ * Return: 0 success
+ */
+int _strcmp(char *s1, char *s2)
+{
+	if (!(strcmp(s1, s2))
+	{
+		return (0);
+	}
+	if (strcmp(s1, s2))
+	{
+		if (s1 > s2)
+		{
+			return (15);
+		}
+		else
+		{
+			return (-15);
+		}
+	}
+}
