30e01195b0b4fce8addc79894a573d3c952651bf
KingHawkins <kinghawkins61@gmail.com>
2022-09-22 00:10:16 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - prototype
+ * @dest: first pointer parameter
+ * @src: second pointer parameter
+ * @n: comparison
+ * Description: copies portion of content of src to dest
+ * Return: 0 success
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		strncpy(*dest, *src, i);
+	}
+	return (*dest);
+}
