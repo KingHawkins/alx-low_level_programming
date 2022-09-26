d92aba64642e245729cbc498089c59ebb35795e8
KingHawkins <kinghawkins61@gmail.com>
2022-09-26 09:58:13 -0700

    task8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string - prototype
+ * @s: pointer to a pointer
+ * @to: pointer parameter
+ * Description: t sets the value of a pointer to a char.
+ */
+void set_string(char **s, char *to)
+{
+	char c;
+	to = &c;
+	s = &to;
+}
