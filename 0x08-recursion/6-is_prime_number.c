3573a93391127bd778fa052757d8104411fe5c00
KingHawkins <kinghawkins61@gmail.com>
2022-09-28 09:56:32 -0700

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,17 +7,18 @@
  */
 int is_prime_number(int n)
 {
-	int i;
+	int i, a;
 
 	for (i = 2; i <= n/2; i++)
 	{
 		if (n % i != 0 && n % 5 != 0)
 		{
-			return (1);
+			a = 1;
 		}
 		else
 		{
-			return (0);
+			a = 0;
 		}
 	}
+	return (a);
 }
