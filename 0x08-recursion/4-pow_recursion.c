427256efa663c4e310ffdff1ff7bc21a7272eb25
KingHawkins <kinghawkins61@gmail.com>
2022-09-28 10:17:44 -0700

    changes

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,8 +9,11 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int p;
-
-	p = pow(x, y);
-	return (p);
+	if (y < 0)
+		return (-1);
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	return (1);
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,37 @@
 #include "main.h"
-#include <math.h>
 /**
- * _sqrt_recursion - prototype
+ * helper - prototype
+ * @i: parameter
  * @n: parameter
  * Return: 0 success else -1
  */
+int helper(int i, int n)
+{
+	int j;
+
+	if (i * i != n)
+	{
+		if (i > n)
+		{
+			return (-1);
+		}
+		j = helper(i + 1, n);
+		return (j + 1);
+	}
+	return (0);
+}
+/**
+ * _sqrt_recursion - returns square root
+ * @n: integer to return
+ * Return: returns int of squareroot
+ */
 int _sqrt_recursion(int n)
 {
-	if (sqrt(n))
-	{
-		return (sqrt(n));
-	}
-	else
+	int i = 0;
+
+	if (helper(i, n) == n && n != 1)
 		return (-1);
+	return (helper(i, n));
 }
+
+
