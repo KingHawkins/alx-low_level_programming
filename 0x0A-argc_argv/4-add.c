91d71174a59d57db77b71de40c5985318d7becdb
KingHawkins <kinghawkins61@gmail.com>
2022-09-30 13:52:11 -0700

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+/**
+ * main - entry point
+ * @argc: argument
+ * @argv: arguement vector
+ * Description: adds two agrc values
+ * Return:0 success
+ */
+int main(int argc, char *argv[])
+{
+	int a, i, j;
+
+	a = 0;
+	if (argc < 2)
+	{
+		putchar('0');
+	}
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+			a  += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
+}
+		
