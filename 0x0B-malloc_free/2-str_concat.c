f1aa796fde0e4850182cde509255dfb91281e8b1
KingHawkins <kinghawkins61@gmail.com>
2022-10-03 07:48:28 -0700

    tasl

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,15 +12,13 @@ char *str_concat(char *s1, char *s2)
 {
 	char *str;
 	int i;
-	char *a;
 
-	a = strcat(s1, s2);
 	str = malloc(sizeof(char) * 4108);
-	for (i = 0; a[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		str[i] = s1[i];
+		printf("%s\n", str);
 	}
-	printf("%s\n", str);
 	free(str);
-	return (str);
+	return (s1);
 }
