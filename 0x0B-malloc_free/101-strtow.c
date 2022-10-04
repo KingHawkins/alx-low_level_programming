e90fa835a6e6b3427aac56d5df3c808bebb85b69
KingHawkins <kinghawkins61@gmail.com>
2022-10-04 03:56:21 -0700

    task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -19,7 +19,7 @@ char **strtow(char *str)
 	{
 		return (NULL);
 	}
-	ptr = malloc(4193);
+	ptr = malloc(sizeof(char) * 4193);
 	if (ptr == NULL)
 	{
 		return (NULL);
@@ -28,8 +28,11 @@ char **strtow(char *str)
 	while (i < n)
 	{
 		ptr[i] = str[i];
-		printf("%c\n", ptr[i]);
 		++i;
+	}
+	if (!isspace(ptr[i]))
+	{
+		printf("%s", ptr);
 	}
 	free(ptr);
 	return ((char **)ptr);
