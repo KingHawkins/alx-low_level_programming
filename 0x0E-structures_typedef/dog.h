6fb9eee777d1b4852c7b321d64e54998fb7c444a
KingHawkins <kinghawkins61@gmail.com>
2022-10-11 06:25:52 -0700

    add

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
+/**
+ * struct dog - structure containing dog info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-	}my_dog;
+};
 
 #endif
