df3834c84f73cdde42fcd3705ef440a3e326964e
KingHawkins <kinghawkins61@gmail.com>
2022-10-13 07:02:14 -0700

    dd

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -18,12 +18,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	{
 		exit(1);
 	}
-
-	if (*action)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			action(*(array + i));
-		}
+		action(*(array + i));
 	}
 }
