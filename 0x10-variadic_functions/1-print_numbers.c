b243b07495c5cd41463e5dbb30c849798c5da46a
KingHawkins <kinghawkins61@gmail.com>
2022-10-14 07:54:08 -0700

    add

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		val = va_arg(vl, double);
 		printf("%f", val);
-		if (separator != NULL && i != n - 1)
+		if (separator != NULL || i != n - 1)
 		{
 			printf("%s", separator);
 		}
