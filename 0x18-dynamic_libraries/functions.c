50e5b57e87c16544b91323841e4fdf86826471c7
KingHawkins <kinghawkins61@gmail.com>
2022-12-13 09:13:08 -0800

    All

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+/**
+ * add - adds
+ * @a: par
+ * @b: par
+ * Description: adds two integers
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts
+ * @a: parameter
+ * @b: parameter
+ * Description: subtracts an int from another
+ * Return: result of sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies
+ * @a: parameter
+ * @b: parameter
+ * Description: multiplies two integers
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division
+ * @a: parameter
+ * @b: parameter
+ * Description: divides two integers
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus
+ * @a: parameter
+ * @b: parameter
+ * Description: gives modulus of a integer
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
